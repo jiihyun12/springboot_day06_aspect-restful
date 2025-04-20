3150dfa6183f71a8dfd844809be1a017118d760e
jiihyun12 <j22h.h0h@gmail.com>
2025-04-20 21:00:00 +0900

    mapper 수정

--- src/main/java/com/app/restful/mapper/PostMapper.java
+++ src/main/java/com/app/restful/mapper/PostMapper.java
@@ -2,12 +2,11 @@ package com.app.restful.mapper;
 
 import com.app.restful.domain.PostDTO;
 import com.app.restful.domain.PostVO;
-import org.apache.ibatis.annotations.Mapper;
+
 
 import java.util.List;
 import java.util.Optional;
 
-@Mapper
 public interface PostMapper {
 
 //    게시글 목록
